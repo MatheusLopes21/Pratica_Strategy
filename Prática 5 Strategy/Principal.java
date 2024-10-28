1ab3868bb08b70e698dac17423fe5bf84a5b5d3d
MatheusLopes <120064706+MatheusLopes21@users.noreply.github.com>
2024-10-27 21:58:16 -0300

    Update Principal.java

--- Prática 5 Strategy/Principal.java
+++ Prática 5 Strategy/Principal.java
@@ -1,3 +1,7 @@
+/* Pratica 5 Strategy Method
+    Dupla: Igor Carvalho Braz -- RA: 42021158
+            Matheus Magalhães Alves Lopes -- RA: 4231922738*/ 
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
